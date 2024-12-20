db48b99205d957f525097a125388ddaad9f5ec18
Hooman <Whooman.t@gmail.com>
2024-12-19 17:08:01 -0800

    Extracting a query object

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import { useState } from "react";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/usePlatform";
 
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+}
+
 const App = () => {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  const [selecctedPlatform, setSelectedPlatform] = useState<Platform | null>(
-    null
-  );
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
   return (
     <Grid
@@ -30,26 +32,20 @@ const App = () => {
       <Show above="lg">
         <GridItem area="aside" paddingX={5} marginTop={2}>
           <GenreList
-            selectedGenre={(genre) => setSelectedGenre(genre)}
-            highlightedGenre={selectedGenre}
+            selectedGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+            highlightedGenre={gameQuery.genre}
           />
         </GridItem>
       </Show>
       <GridItem area="main">
         <PlatformSelector
-          onSelect={(platfrom) => setSelectedPlatform(platfrom)}
-          selectedPlatform={selecctedPlatform}
+          onSelect={(platform) => setGameQuery({ ...gameQuery, platform })}
+          selectedPlatform={gameQuery.platform}
         />
-        <GamesGrid
-          selectedGenre={selectedGenre}
-          selectedPlatform={selecctedPlatform}
-        />
+        <GamesGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
   );
 };
 
 export default App;
-function usestate<T>(): [any, any] {
-  throw new Error("Function not implemented.");
-}

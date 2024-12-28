02b3c25d43f164a2230c8cd5ab3fefedf6f57013
Hooman <Whooman.t@gmail.com>
2024-12-27 17:48:24 -0800

    Handle route errors

--- src/routing/route.tsx
+++ src/routing/route.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../pages/Layout";
 import HomePage from "../pages/HomePage";
 import GamesDetailPage from "../pages/GamesDetailPage";
+import ErrorPage from "../pages/ErrorPage";
 
 const route = createBrowserRouter([
   {
     path: "/",
+    errorElement: <ErrorPage />,
     element: <Layout />,
     children: [
       { index: true, element: <HomePage /> },

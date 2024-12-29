647e9dd51d62e1e0fc19a08b639a08a544a67bd2
Hooman <Whooman.t@gmail.com>
2024-12-28 17:14:52 -0800

    Build expandable text

--- src/pages/GamesDetailPage.tsx
+++ src/pages/GamesDetailPage.tsx
@@ -1,19 +1,36 @@
 import { useParams } from "react-router-dom";
 import useGameDetails from "../hooks/useGameDetails";
-import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 const GamesDetailPage = () => {
   const { slug } = useParams();
   const { data, isLoading, error } = useGameDetails(slug!);
+  const limit = 350;
+  const [isExpanded, setExpanded] = useState(false);
 
   if (isLoading) return <Spinner />;
 
   if (error) throw error;
 
+  const summary = isExpanded
+    ? data?.description_raw
+    : data?.description_raw.slice(0, limit);
+
   return (
     <Box padding={5}>
       <Heading>{data?.name}</Heading>
-      <Text>{data?.description_raw}</Text>
+      <Text>
+        {isExpanded ? `${summary}  ` : `${summary}...  `}
+        <Button
+          size={"xs"}
+          fontWeight="bold"
+          colorScheme="yellow"
+          onClick={() => setExpanded(!isExpanded)}
+        >
+          {isExpanded ? "Show less" : "Show more"}
+        </Button>
+      </Text>
     </Box>
   );
 };

7b31b224a65aa5b39acabc7df6508ea204abc260
Hooman <Whooman.t@gmail.com>
2024-12-30 16:17:49 -0800

    Refactor entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
-import { Game } from "../entities/Game";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
+import Game from "../entities/Game";
 import PlatformIconsList from "./PlatformIconsList";
 import CriticScore from "./CriticScore";
 import getCroppedImage from "../services/url-image";

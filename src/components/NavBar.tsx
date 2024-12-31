4fd2c96db9639e822998ea27fcbf8809c49c00f9
Hooman <Whooman.t@gmail.com>
2024-12-30 16:08:01 -0800

    Fix the search input (navbar)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,23 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Link to="/">
+        <Image
+          src={logo}
+          boxSize="60px"
+          _hover={{
+            cursor: "pointer",
+            transform: "scale(1.1)",
+          }}
+          transition="all 0.1s ease-in-out"
+          objectFit="cover"
+        />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
